748bef82515f617699c88455143b79d6056a034a
chiheblahouli <chiheblahhouli@gmail.com>
2020-02-11 22:41:06 +0000

    3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -26,19 +26,15 @@ if (year % 4 == 0)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
-	
-		else
+	}
+	else
+	{
+		if (month >= 2 && day >= 60)
 		{
-			if (month == 2 && day == 60)
-			{
-				printf("Invalid date: %02d/%02d/%04d\n", month, day -31, year);
-			}
-			else
-			{
-				printf("Day of the year: %d\n", day);
-				printf("Remaining days: %d\n", 365 - day);
-			}
+			day++;
 		}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 }
 else
