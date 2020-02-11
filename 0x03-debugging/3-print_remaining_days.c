68b6635c78d90a3d21948860f74ce5311fb555b5
chiheblahouli <chiheblahhouli@gmail.com>
2020-02-11 22:00:42 +0000

    3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,15 +27,7 @@ if (year % 4 == 0)
 			printf("Remaining days: %d\n", 366 - day);
 		}
 	}
-	else
-	{
-		if (month >= 2 && day >= 60)
-		{
-			day++;
-		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
+	
 }
 else
 {
