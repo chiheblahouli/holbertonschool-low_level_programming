f7f8aa5e48b2bd57f45a316f7bd074a6733c984d
chiheblahouli <chiheblahhouli@gmail.com>
2020-02-11 20:52:28 +0000

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - check the code for Holberton School students.
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char x;
+for (i = 0 ; i < 10 ; i++)
+{
+for (x = 'a'; x <= 'z' ; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
+}
