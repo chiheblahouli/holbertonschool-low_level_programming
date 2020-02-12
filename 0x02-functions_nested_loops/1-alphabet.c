2e5d73d15d4d9c8fad03cc16ad92d44399bff9d7
chiheblahouli <chiheblahhouli@gmail.com>
2020-02-12 00:25:53 +0000

    2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
+
 /**
  * print_alphabet - check the code for Holberton School students.
  *
  * Return: void.
  */
-void print_alphabet(void) {
-char x;
 
-for (x = 'a'; x <= 'z' ; x++) {
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
 	_putchar(x);
 }
-
 _putchar('\n');
-
 }
