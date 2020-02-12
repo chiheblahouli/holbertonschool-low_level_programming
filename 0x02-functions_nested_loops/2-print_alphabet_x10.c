133cb6777be5b95633c96ff2a21eeea14e57a34b
chiheblahouli <chiheblahhouli@gmail.com>
2020-02-12 00:30:31 +0000

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,9 @@
 void print_alphabet_x10(void)
 {
 int i;
-char x;
-for (i = 0 ; i < 10 ; i++)
+for (i = 0; i < 10; i++)
 {
-for (x = 'a'; x <= 'z' ; x++)
-{
-_putchar(x);
+	print_alphabet()
 }
 _putchar('\n');
-}
 }
