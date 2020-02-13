d3604f6fb88850fec24723c3c15dd6ab1dd42fd0
chiheblahouli <chiheblahhouli@gmail.com>
2020-02-13 14:44:41 +0000

    fix

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _isupper - Check for uppercase character
+ * @c: character to be checked
+ *
+ * Return: 1 if is uppercase, 0 otherwise
+ */
+int _isupper(char c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
