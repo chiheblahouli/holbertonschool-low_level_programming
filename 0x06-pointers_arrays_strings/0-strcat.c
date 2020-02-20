9934311123111fdaffdc709a68c04410684dbc57
chiheblahouli <chiheblahhouli@gmail.com>
2020-02-20 13:50:56 +0000

    x

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,13 @@
 */
 char *_strcat(char *dest, char *src)
 {
-
-
+int i;
+int l;
 for (l = 0; dest[l] != '\0'; l++)
 {
 }
 for (i = 0; src[i] != '\0'; i++)
-{
-dest [l+i] = '\0';
-}
+dest[l + i] = src[i]
+dest[l + i] = '\0';
 return (dest);
 }
