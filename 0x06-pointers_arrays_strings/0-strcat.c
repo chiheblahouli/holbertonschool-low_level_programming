69f5ecad1a8d0e353e45b165621caf15826855b2
chiheblahouli <chiheblahhouli@gmail.com>
2020-02-20 13:42:11 +0000

    x

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,2 +1,21 @@
 #include "holberton.h"
+/**
+* _strcat - concatenates two strings.
+* @src : source
+* @dest : destination
+*
+*return : dest
+*/
+char *_strcat(char *dest, char *src)
+{
 
+
+for (l = 0; dest[l] != '\0'; l++)
+{
+}
+for (i = 0; src[i] != '\0'; i++)
+{
+dest [l+i] = '\0';
+}
+return (dest);
+}
