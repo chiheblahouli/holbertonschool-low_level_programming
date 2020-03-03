4dbb48673d5764090c9658671181d9002c75561e
chiheb lahouli <chiheb.lahouli@gmail.com>
2020-03-03 14:10:48 +0100

    array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* create_array - creates an array of chars.
+* @size: parameter
+* @c: parameter
+*
+* Return: Nothing.
+*/
+char *create_array(unsigned int size, char c)
+{
+}
