79ad7fd4a6f67053ce31e0069d31aeba541af657
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-04 12:58:53 +0000

    c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* *_strdup - prints a grid of integers
-* @str: string.
-* Return: m.
-*/
+ * *_strdup - prints a grid of integers
+ * @str: string.
+ * Return: m.
+ */
 char *_strdup(char *str)
 {
 int i;
-char *m;
+char *d;
 if (str == NULL)
 return (NULL);
 for (i = 0; str[i]; i++)
-m = malloc((i + 1) * sizeof(char));
-if (m == NULL)
+d = malloc((i + 1) * sizeof(char));
+if (d == NULL)
 return (NULL);
 for (i = 0; str[i]; i++)
 {
-m[i] = str[i];
-m[i] = '\0';
-return (m);
+d[i] = str[i];
 }
+d[i] = '\0';
+return (d);
 }
