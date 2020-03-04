89ad54790759d7de612229658ae65a1ae50b114d
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-04 21:00:37 +0000

    3grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * alloc_grid - function returns a pointer to a 2 dimensional array of integers.
+ * alloc_grid - function returns a pointer to a 2-dimensional array of integers.
  * @width: width.
  * @height: height.
- * Return: Grid of integers.
+ * Return: Grid of integers initialized to 0.
 */
 
 int **alloc_grid(int width, int height)
@@ -12,22 +13,19 @@ int **alloc_grid(int width, int height)
 int i, j, **g;
 if (width <= 0 || height <= 0)
 		return (NULL);
-g = malloc(sizeof(int *) * height);
+g = malloc(sizeof(int) * height);
 if (g == NULL)
 	return (NULL)
-else
+for (i = 0; i < height; i++)
 {
-	for (i = 0; i < height; i++)
+	g[i] = malloc(sizeof(int) * width);
+	if (g[i] == NULL)
+		return (NULL)
+	else
 	{
-		g[i] = malloc(sizeof(int) * width);
-		if (g[i] == NULL)
-              		return (NULL)
-		else
-		{
-			for (j = 0; j < width; j++)
-			g[i, j] = 0;
-		}
+		for (j = 0; j < width; j++)
+		g[i, j] = 0;
 	}
-	return (g)
 }
+return (g)
 }
