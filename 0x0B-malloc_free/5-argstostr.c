b0a5ac337fe81531bc05c436a3e8fcf5c4e78ae7
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-04 22:10:38 +0000

    5argstostr

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -7,37 +7,33 @@
  * Return: pointer to new string.
 */
 
-
 char *str_concat(char *s1, char *s2)
 {
-
 int i = 0, j = 0, k;
 char *d;
-
 if (s1 == NULL)
-        s1 = "";
+	s1 = "";
 if (s2 == NULL)
-        s2 = "";
+	s2 = "";
 while (s1[i] != '\0')
-        i++;
+	i++;
 while (s2[j] != '\0')
-        j++;
+	j++;
 d = malloc(sizeof(char) * (i + j + 1));
 if (d == NULL)
-        return (NULL);
+	return (NULL);
 if (s1 != '\0' && s2 != '\0')
 {
-        for (k = 0; k < i; k++)
-                d[k] = s1[k];
-        for (k = i; k < i + j; k++)
-                d[k] = s2[k - i];
-        d[k] = '\0';
+	for (k = 0; k < i; k++)
+		d[k] = s1[k];
+	for (k = i; k < i + j; k++)
+		d[k] = s2[k - i];
+	d[k] = '\0';
 }
 else
-        d = "";
+	d = "";
 return (d);
 }
-
 
 char *argstostr(int ac, char **av)
 {
