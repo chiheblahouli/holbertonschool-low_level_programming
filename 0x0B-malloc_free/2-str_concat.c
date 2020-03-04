c0a03a89c954cf05000a395d686e2eef3a0daaee
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-04 20:16:12 +0000

    2concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,13 +1,11 @@
 #include "holberton.h"
 #include <stdlib.h>
-
 /**
-* _str_concat - a function concatenating two strings
-* @s1: input string1
-* @s2: input string2
-* Return: d string
-*/
-
+ * str_concat - concatenates two strings.
+ * @s1: string 1.
+ * @s2: string 2.
+ * Return: concatenated string.
+ */
 
 char *str_concat(char *s1, char *s2)
 {
@@ -23,15 +21,15 @@ while (s1[i] != '\0')
 	i++;
 while (s2[j] != '\0')
 	j++;
-d = malloc((i + j + 1) * sizeof(char));
+d = malloc(sizeof(char) * i + j + 1);
 if (d == NULL)
 	return (NULL);
 if (s1 != '\0' && s2 != '\0')
 {
 	for (k = 0; k < i; k++)
-		d[k] = s1[k];
-	for (k = i; k < i + j; k++)
-		d[k] = s2[k - i];
+		d[k] = *s1[k];
+	for (k = 0; k < j; k++)
+		d[k + i] = *s2[k];
 	d[k] = '\0';
 }
 else
