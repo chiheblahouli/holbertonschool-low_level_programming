c8e9c303955d13a33c8734465d01c0e9fb0d6619
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-04 19:27:37 +0000

    2concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,11 +11,15 @@
 
 char *_str_concat(char *s1, char *s2)
 {
+
 int i = 0, j = 0, k;
 char *d;
 
-if (s1 == NULL || s2 == NULL)
-        return (NULL);
+if (s1 == NULL)
+	s1 = "";
+
+if (s2 == NULL)
+	s2 = "";
 
 while (s1[i] != '\0')
         i++;
