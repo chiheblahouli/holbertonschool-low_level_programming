ad22bb0e19a9adecca5cecf209349e05a5b214bb
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-05 22:38:24 +0000

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,31 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *string_nconcat - function
- *@s1: string.
- *@s2: string.
- *@n: integer.
- *Return: string.
+ * string_nconcat -concat string
+ *@s1: string
+ *@s2:  string
+ *@n: first byte
+ *Return: NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, l1, l2, j;
-	char s;
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (l1 = 0; s1[l1] != '\0'; l1++)
-	for (l2 = 0; s2[l2] != '\0'; l2++)
-	if (n >= l2)
-	n = l2;
-	s = malloc(sizeof(char) * (l1 + n + 1));
+	char *s;
+	unsigned int i = 0, j = 0, m = 0, c = 0;
+if (s1 == NULL)
+	s1 = "";
+	for (i = 0; s1[i]; i++)
+	;
+if (s2 == NULL)
+s2 = "";
+for (j = 0; s2[j]; j++)
+;
+	if (j < n)
+	n = j;
+	s = malloc((i + n + 1) * sizeof(char));
 	if (s == NULL)
 	return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-	s[i] = s1[i];
-	for (j = l1; j < l1 + n; j++)
-	s[j] = s2[j - l1];
-	s[j] = '\0';
+	for (m = 0; m < i  ; l++)
+	s[l] = s1[l];
+	for (c = 0; c < n; c++)
+	s[m + c] = s2[c];
+	s[m + c] = '\0';
 	return (s);
 }
