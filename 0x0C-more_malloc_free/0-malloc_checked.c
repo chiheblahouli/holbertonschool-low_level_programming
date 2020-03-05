bb40e1d526a885fcc4ef779f4eb919796be95037
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-05 17:24:39 +0000

    checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- **malloc_checked - a function that allocates memory using malloc.
- *@b : parameter.
- *Return: ALways 0.
+ * malloc_checked - a function that allocates memory using malloc.
+ * @b : parameter.
+ * Return: ALways 0.
  */
 void *malloc_checked(unsigned int b)
 {
