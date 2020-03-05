7f1d6ce277e51f3b5d7a7d743638eb9747b92512
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-05 22:16:34 +0000

    c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,32 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *string_nconcat - a function that concatenates two strings.
- *@s1 : destination of string.
- *@s2 : source of string.
- *@n : int.
- *Return: Always 0.
+ *string_nconcat - function
+ *@s1: char
+ *@s2: char
+ *@n: int
+ *Return: char
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int e, m, j, i;
 	char *s;
+	unsigned int i, o = k, l2 = 0;
 
 	if (s1 == NULL)
-	s1 = "";
+			s1 = "";
 	if (s2 == NULL)
-	s2 = "";
-	for (e = 0; s1[e] != '\0'; e++)
-	for (m = 0; s2[m] != '\0'; m++)
-	if (n >= m)
-	n = m;
-	s = malloc(sizeof(char) * (e + n + 1));
+			s2 = "";
+	while (s1[o] != '\0')
+			o++;
+	while (s2[k] != '\0')
+			k++;
+	if (n >= l2)
+			n = l2;
+			s = malloc(o + n + 1);
 	if (s == NULL)
-	return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-	s[i] = s1[i];
-	for (j = e; j < e + n; j++)
-	s[j] = s2[j - e];
-	s[j] = '\0';
-	return (s);
+		return (s);
+	for (i = 0; i < o; i++)
+			s[i] = s1[i];
+	for (i = 0; i < n; i++)
+			s[o + i] = s2[i];
+	s[o + i] = '\0';
+			return (s);
 }
