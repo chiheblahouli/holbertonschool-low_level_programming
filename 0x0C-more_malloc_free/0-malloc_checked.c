32c0497d2932be927548d187db297b26b47bded5
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-05 17:13:53 +0000

    checked.

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,24 +1,19 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-
 /**
- * malloc_checked - a function that allocates memory using malloc
- * @b: parameter
- * Return: Always 0.
+ * malloc_checked - func allocates memory using malloc
+ *@b: input
+ *Return: void pointer
  */
-	void *malloc_checked(unsigned int b)
+void *malloc_checked(unsigned int b)
 {
-unsigned int *p, j;
-for (j = 0; j < b; j++)
-;
-p = malloc(sizeof(unsigned int) * b);
-;
-if (!p)
-return (NULL);
+char *p;
 
+		m = malloc(b);
+		if (p == NULL)
+
+		exit(98);
+
+		else
+
+		return (p);
 }
-		free(p);
-			return (p);
-					}
