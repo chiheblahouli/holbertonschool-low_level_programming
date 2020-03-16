1f1a13cb09cc10b9aa50f5341886afb15db156c1
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-16 16:24:27 +0000

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,8 +16,11 @@ int i;
 		return (-1);
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
+	{
 		if (cmp(array[i]) != 0)
 			return (i);
+	}
 			return (-1);
 }
+
