d2d2cc873076df4d28f69dbc6bc38868d4f586a8
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-16 16:06:48 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ struct dog
 };
 typedef struct dog dog_t;
 int _putchar(char c);
-
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
