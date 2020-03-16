3dfb96267a4d84d2750bc46c2ce3e7cd4c74c5ed
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-16 16:09:58 +0000

    new

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * new_dog - creates dogs.
+ * @name: st.
+ * @age: float.
+ * @owner: st.
+ * Return: dog.
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	return (NULL);
+}
