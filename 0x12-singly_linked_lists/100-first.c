6f3f6c81b8abe8becf2804f07ca45f79c3eaee67
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-22 17:50:41 +0000

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * Before_execution - printf before the main.
+ *
+ * Return: Always 0.
+ */
+void before_execution(void)
+
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
