691db5ff88ef649fd68798e9b15cdb23a633dc00
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-22 10:37:05 +0000

    print

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - function that prints all elements
+ * @h: pointer.
+ *
+ * Return: number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+
+		unsigned long i;
+
+	for (i = 0; h; i++)
+	{
+	while (h != NULL)
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		}
+		return (i);
+}
