cd430a420bc9fb8a742ecca85675b5e8eb6ebcbd
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-24 12:54:26 +0000

    c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,11 +2,12 @@
 /**
  * free_listint2 - frees a listint_t list.
  * @head: the first node
- * Return: Always 0.
+ *
  */
 void free_listint2(listint_t **head)
 {
 listint_t *cto;
+
 if (head != NULL)
 {
 while (*head != NULL)
