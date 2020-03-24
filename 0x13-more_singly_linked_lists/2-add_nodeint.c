6d038daec12e3a065ab5e3c7c2d613e6e490e12a
chiheblahouli <chiheblahhouli@gmail.com>
2020-03-24 12:25:52 +0000

    c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list.
- * new_node: parameter.y
+ * new_node: parameter.
  * @head: pointer.
  * @n: integer.
  * Return: Always 0.
