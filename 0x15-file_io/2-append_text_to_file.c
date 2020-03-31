3286a8d19eb2342496f0d2451aa27d3559c12034
chiheblahouli <chiheblahouli@gmail.com>
2020-03-31 17:52:35 +0000

    c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,12 @@
+
+#include "holberton.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename : file name.
+ * @text_content : text content.
+ * Return: Always 0.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
