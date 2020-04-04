b30b33ea5f9540164685eb1d8d2f976c9ec201f0
chiheblahouli <chiheblahouli@gmail.com>
2020-04-04 10:06:35 +0000

    c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,49 @@
 #include "holberton.h"
 /**
- * print_tables - prints tables.
+ * print_times_table - prints tables.
  * @n: integer.
  * Return: Always 0.
  */
-void print_tables(int n)
+void print_times_table(int n)
 {
-int i, j, x1, x2, y1, y2, Ãz;
+{
+	int x = 0, y, z;
 
-for (i = 0 ; i <= n ; i++)
-{
-for (j = 0 ; j <= 9 ; j++)
-{
-x1 = i * j;
-x2 = i * (j + 1);
-y1 = x1 / 10;
-y2 = x2 / 10;
-z = x1 % 10;
-if (y1 == 0)
-{
-_putchar(z + '0');
-if (j < 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-if ((y2 == 0) && (j < 9))
-_putchar(' ');
-}
-else
-{
-_putchar(y1 + '0');
-_putchar(z + '0');
-if (j < 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-}
-_putchar('\n');
-}
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		x++;
+	}
 }
