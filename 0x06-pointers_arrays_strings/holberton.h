4e014bcacd384ae54ee7233c7286a406e23a9939
chiheblahouli <chiheblahouli@gmail.com>
2020-04-04 10:55:36 +0000

    c

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,9 +1,17 @@
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
+
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char c);
 char *rot13(char *);
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
 void print_number(int n);
+
+#endif
