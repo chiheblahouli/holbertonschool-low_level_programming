4cc5b83f6571ad3194fe3a063c226b641ba39140
chiheblahouli <chiheblahouli@gmail.com>
2020-04-11 12:39:05 +0000

    c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,79 +1,41 @@
+
 #include "holberton.h"
 /**
- * print_number - prints a number
- * @n: number to print
+ * print_number - prints an integer.
+ * @n: No parameters
+ * Return: void
  */
+
 void print_number(int n)
 {
-	int i;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		for (i = 0; i < getlen(n); i++)
-			_putchar('0' + -getnum(n, i));
-	}
-	else
-	{
-		for (i = 0; i < getlen(n); i++)
-			_putchar('0' + getnum(n, i));
-	}
-}
-
-/**
- * _pow - Calculates exponents
- * @a: base
- * @b: exponent
- *
- * Return: a^b
- */
-int _pow(int a, int b)
+int x, j, i = 0, n1, n2 = n, k = 1;
+if (n == 0)
+_putchar(48);
+else
 {
-	int i, ans;
-
-	if (b <= 0)
-		return (1);
-
-	ans = a;
-	for (i = 0; i < b - 1; i++)
-	{
-		ans *= a;
-	}
-
-	return (ans);
-}
-
-/**
- * getnum - gets the 'index'th digit of num, starting from the left at 0
- * @num: the number to extract digit from
- * @index: the index of the desired digit with 0 being the leftmost digit
- *
- * Return: The 'index'th digit of num
- */
-int getnum(int num, int index)
+if (n < 0)
 {
-	int l;
-
-	l = getlen(num);
-	num = num / _pow(10, l - 1 - index);
-	return (num % 10);
+_putchar('-');
+if (n == -2147483648)
+n += 1;
+n = -n;
 }
-
-/**
- * getlen - gets the length of the number num
- * @num: the number to get length of
- *
- * Return: length of number 'num'
- */
-int getlen(int num)
+n1 = n;
+while (n > 0)
 {
-	int i;
-
-	i = 1;
-	while (num > 9 || num < -9)
-	{
-		i++;
-		num /= 10;
-	}
-	return (i);
+n = n / 10;
+i++;
+if (i > 1)
+k *= 10;
+}
+for (j = 0 ; j < i ; j++)
+{
+x = n1 / k;
+if (n2 == -2147483648 && j == i - 1)
+x += 1;
+_putchar(x + '0');
+n1 = n1 % k;
+k /= 10;
+}
+}
 }
