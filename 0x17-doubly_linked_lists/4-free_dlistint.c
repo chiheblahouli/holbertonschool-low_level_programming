e236f804cfca98890a8886cb72f78c7d73ddc256
chiheblahouli <chiheblahouli@gmail.com>
2020-05-03 15:16:43 +0000

    c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees doubly linked list
+ * @head: pointer.
+**/
+void free_dlistint(dlistint_t *head)
+{
+	if (!head)
+		return;
+	while (head->next)
+{
+	head = head->next;
+	free(head->prev);
+}
+
+	free(head);
+}
