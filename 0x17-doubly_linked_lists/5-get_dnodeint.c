25903457fba8826cb2dd56de04a48cdd590b8d25
chiheblahouli <chiheblahouli@gmail.com>
2020-05-03 15:19:49 +0000

    c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - gets nodes.
+ * @head: pointer.
+ * @index: index of node.
+ * Return: NULL if failed.
+**/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	size_t i = 0;
+
+	while (index > head && i)
+	{
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
