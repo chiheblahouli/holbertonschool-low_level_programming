5dd33792c88642c172c294705a2b05036b5aa751
chiheblahouli <chiheblahouli@gmail.com>
2020-05-03 14:50:32 +0000

    c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlisti0nt - prints all the elements of dlistint_t.
+ * @h: pointer.
+ * Return: 0 if nothing.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (!h)
+	return (0);
+	while (h)
+{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+}
+	return (i);
+}
