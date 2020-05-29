e1c926e946ecd5ff2562b1e128e9dbedf50cea7e
chiheblahouli <chiheblahouli@gmail.com>
2020-05-29 19:01:08 +0000

    c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes the hash tables.
+ * @ht: pointer to created a hash table
+ **/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *index;
+	hash_node_t *node;
+
+	if (!ht)
+			return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+			index = ht->array[i];
+	while (index)
+	{
+		node = index->next;
+		free(index->key);
+		free(index->value);
+		free(index);
+		index = node;
+	}
+	}
+	free(ht->array);
+	free(ht);
+}
