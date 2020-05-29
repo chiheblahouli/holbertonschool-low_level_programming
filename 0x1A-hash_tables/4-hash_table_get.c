0db6251bfe4735e4f07b55005649c3573e13e8ba
chiheblahouli <chiheblahouli@gmail.com>
2020-05-29 18:51:15 +0000

    c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - returns the value of a given key.
+ * @ht: The hash table to retrieve the  key.
+ * @key: The key to retrieve the value.
+ * Return: returns the pointer of the value.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || ht->array == NULL)
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		node = node->next;
+	}
+
+	return (NULL);
+}
