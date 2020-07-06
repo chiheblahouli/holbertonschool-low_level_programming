0519057bb801ea6f987f890345b4dfd919b19eeb
chiheblahouli <chiheblahouli@gmail.com>
2020-07-06 12:51:00 +0000

    c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add element to a hash table
+ *@ht: the hash table to add
+ *@key: password.
+ *@value: Mean.
+ * Return: Always 0.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *hn;
+	hash_node_t *tmp;
+	unsigned long int i = 0;
+
+	if (ht == NULL || key == NULL)
+		return (0);
+
+	i = key_index((void *)key, ht->size);
+	tmp = ht->array[i];
+
+
+	while (tmp)
+	{
+		if (strcmp((const char *)key, tmp->key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+
+	if (tmp == NULL)
+	{
+		hn = malloc(sizeof(hash_node_t));
+		if (!hn)
+			return (0);
+
+		hn->key = strdup(key);
+		hn->value = strdup(value);
+		hn->next = ht->array[i];
+		ht->array[i] = hn;
+		return (1);
+	}
+
+
+	return (1);
+}
